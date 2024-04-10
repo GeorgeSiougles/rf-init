c693ed8751d38c735634f83487d15ec66180a54c
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-04-10 14:59:37 +0300

    Add store to application

---
+++ src/store/charactersSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const charactersSlice = createSlice({
+  name: 'characters',
+  initialState: {
+    list: [],
+    currentCharacterIndex: 0,
+  },
+  reducers: {
+    addCharacter(state, action) {
+      state.list.push(action.payload);
+    },
+    removeCharacter(state, action) {
+      state.list = state.list.filter(
+        (character) => character.id !== action.payload
+      );
+    },
+    sortCharacters(state) {
+      state.list.sort((a, b) => {
+        const initiativeA = a.rolledInitiative + a.bonus;
+        const initiativeB = b.rolledInitiative + b.bonus;
+        return initiativeB - initiativeA;
+      });
+    },
+    increaseIndex(state, action) {
+      console.log('increase');
+      state.currentCharacterIndex = state.currentCharacterIndex + 1;
+    },
+    resetIndex(state, action) {
+      console.log('reset');
+      state.currentCharacterIndex = 0;
+    },
+  },
+});
+
+export const {
+  addCharacter,
+  removeCharacter,
+  sortCharacters,
+  increaseIndex,
+  resetIndex,
+} = charactersSlice.actions;
+
+export default charactersSlice.reducer;
