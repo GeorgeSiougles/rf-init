eba6bd4b877271e84bfa5b049bc1fb16604c1c3b
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-04-30 16:19:24 +0300

    Fix condition styles

--- src/components/Conditions/CurrentConditions.jsx
+++ src/components/Conditions/CurrentConditions.jsx
@@ -2,6 +2,7 @@ import { Minus } from 'lucide-react';
 import { useDispatch } from 'react-redux';
 import { removeCondition } from '../../store/charactersSlice';
 import { conditionsWithDescription } from '../../utlis/allConditions';
+import ConditionDrawer from './ConditionDrawer';
 
 const CurrentConditions = ({ id, conditions }) => {
   const dispatch = useDispatch();
@@ -17,18 +18,40 @@ const CurrentConditions = ({ id, conditions }) => {
     dispatch(removeCondition({ characterId: id, conditionName: condition }));
   };
   return (
-    <div>
+    <div className="flex">
       {conditions.map((condition, index) => (
-        <p className="tooltip" data-tip={condition.description} key={index}>
-          {condition.condition}:{condition.duration}
+        <div
+          className="tooltip btn badge"
+          data-tip="Click for more info"
+          key={index}
+        >
+          <ConditionDrawer
+            condition={condition.condition}
+            duration={condition.duration}
+            description={condition.description}
+          />
           <button
+            className="tooltip"
+            data-tip="Click - to delete"
             onClick={() => handleRemoveConditionClick(condition.condition)}
           >
             <Minus />
           </button>
-        </p>
+        </div>
       ))}
     </div>
+    // <div>
+    //   {conditions.map((condition, index) => (
+    //     <p className="tooltip" data-tip={condition.description} key={index}>
+    //       {condition.condition}:{condition.duration}
+    //       <button
+    //         onClick={() => handleRemoveConditionClick(condition.condition)}
+    //       >
+    //         <Minus />
+    //       </button>
+    //     </p>
+    //   ))}
+    // </div>
   );
 };
 export default CurrentConditions;
