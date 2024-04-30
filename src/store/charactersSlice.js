388ad8d5acab43bbee71357b5eb084c8260b62e7
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-04-30 10:28:03 +0300

    Implement condition and end turn reducers

--- src/store/charactersSlice.js
+++ src/store/charactersSlice.js
@@ -35,6 +35,70 @@ const charactersSlice = createSlice({
     resetIndex(state) {
       state.currentCharacterIndex = 0;
     },
+    addCondition(state, action) {
+      const { characterId, condition } = action.payload;
+      console.log('Rendred at addCondition from action.payload:');
+      console.log('characterId:', characterId);
+      console.log('condition:', condition);
+
+      const character = state.list.find((char) => char.id === characterId);
+      if (character) {
+        console.log('Character found from id:', character);
+        const existingConditionIndex = character.conditions.findIndex(
+          (cond) => cond.condition === condition.condition
+        );
+        if (existingConditionIndex !== -1) {
+          console.log('Condition found, increase duration');
+          // If condition already exists, increase its duration
+          character.conditions[existingConditionIndex].duration +=
+            condition.duration;
+        } else {
+          // Otherwise, add new condition
+          console.log('Condition not found, adding new condition');
+          character.conditions.push(condition);
+        }
+      }
+    },
+    // addCondition(state, action) {
+    //   const { characterId, condition } = action.payload;
+    //   console.log('Rendred at addCondition from action.payload:');
+    //   console.log('characterId:', characterId);
+    //   console.log('condition:', condition);
+
+    //   const character = state.list.find((char) => char.id === characterId);
+    //   if (character) {
+    //     console.log('Character found from id:', character);
+    //     const existingConditionIndex = character.conditions.findIndex(
+    //       (cond) => {
+    //         console.log('Looking for existing condition:', cond.condition);
+    //         cond.condition === condition.condition;
+    //       }
+    //     );
+    //     if (existingConditionIndex !== -1) {
+    //       console.log('Condition found increase duration');
+    //       // If condition already exists, increase duration
+    //       character.conditions[existingConditionIndex].duration +=
+    //         condition.duration;
+    //     } else {
+    //       // Otherwise, add new condition
+    //       console.log('Condition not found');
+    //       character.conditions.push(condition);
+    //     }
+    //   }
+    // },
+    removeCondition(state, action) {},
+    endTurn(state) {
+      state.list.forEach((character) => {
+        character.conditions.forEach((condition) => {
+          console.log('Rendered from endTurn');
+          console.log('Duration left:', condition.duration);
+          condition.duration--;
+        });
+        character.conditions = character.conditions.filter(
+          (condition) => condition.duration > 0
+        );
+      });
+    },
   },
 });
 
@@ -44,6 +108,9 @@ export const {
   sortCharacters,
   increaseIndex,
   resetIndex,
+  addCondition,
+  removeCondition,
+  endTurn,
 } = charactersSlice.actions;
 
 export default charactersSlice.reducer;
