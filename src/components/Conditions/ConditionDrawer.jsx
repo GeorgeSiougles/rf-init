8bc69777ced8c737406316ab4e3cfcb3840f8ef6
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-04-30 16:19:42 +0300

    Add condition drawer with description

---
+++ src/components/Conditions/ConditionDrawer.jsx
@@ -0,0 +1,25 @@
+const ConditionDrawer = ({ condition, duration, description }) => {
+  return (
+    <div className="drawer drawer-end">
+      <input id={`${condition}`} type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <label htmlFor={`${condition}`} className="badge">
+          {condition} : {duration}
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor={`${condition}`}
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          <li>
+            <a>{description}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default ConditionDrawer;
