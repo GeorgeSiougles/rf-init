efffcbe49874f53a1e02a791c019c7c6f4a1ac32
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-04-30 15:47:13 +0300

    display condition description

--- src/components/Conditions/CurrentConditions.jsx
+++ src/components/Conditions/CurrentConditions.jsx
@@ -1,6 +1,7 @@
 import { Minus } from 'lucide-react';
 import { useDispatch } from 'react-redux';
 import { removeCondition } from '../../store/charactersSlice';
+import { conditionsWithDescription } from '../../utlis/allConditions';
 
 const CurrentConditions = ({ id, conditions }) => {
   const dispatch = useDispatch();
@@ -9,12 +10,16 @@ const CurrentConditions = ({ id, conditions }) => {
       'Called before dispatch(removeCondition)) for condition name:',
       condition
     );
+    console.log(
+      'Attemt to access the condition description:',
+      conditionsWithDescription[condition.condition]
+    );
     dispatch(removeCondition({ characterId: id, conditionName: condition }));
   };
   return (
     <div>
       {conditions.map((condition, index) => (
-        <p key={index}>
+        <p className="tooltip" data-tip={condition.description} key={index}>
           {condition.condition}:{condition.duration}
           <button
             onClick={() => handleRemoveConditionClick(condition.condition)}
